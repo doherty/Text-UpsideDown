30f961abc9eeb4895b4e2177440b11969d297260
Marcel Gruenauer <hanekomu@gmail.com>
2008-07-25 14:17:12 +0000

    continued to update distribution to standard style
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Text-UpsideDown/trunk@16523 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Text/UpsideDown.pm
+++ lib/Text/UpsideDown.pm
@@ -91,7 +91,7 @@ sub upside_down {
 
 __END__
 
-{% USE p = PodGenerated %}
+
 
 =head1 NAME
 
@@ -117,7 +117,34 @@ mathematics and the like.
 
 =back
 
-{% PROCESS standard_pod %}
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHORS
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007-2008 by the authors.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
 =cut
 
