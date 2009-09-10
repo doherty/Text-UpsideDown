d605250b15c4500a537a95b028e8f500455c0a9e
Marcel Gruenauer <hanekomu@gmail.com>
2009-09-10 14:07:33 +0200

    repo maintenance

--- lib/Text/UpsideDown.pm
+++ lib/Text/UpsideDown.pm
@@ -2,6 +2,7 @@ package Text::UpsideDown;
 
 use warnings;
 use strict;
+use 5.008;
 use charnames ':full';
 use base 'Exporter';
 
@@ -140,7 +141,7 @@ Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007-2008 by the authors.
+Copyright 2007-2009 by the authors.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
