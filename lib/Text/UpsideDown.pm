96ab7117b2654da9359456a7767a4e5d9eecd50b
Mike Doherty <doherty@cs.dal.ca>
2011-07-07 21:28:56 -0300

    Expand and improve letter inversions

--- lib/Text/UpsideDown.pm
+++ lib/Text/UpsideDown.pm
@@ -1,32 +1,31 @@
+package Text::UpsideDown;
 use 5.008;
 use strict;
 use warnings;
-
-package Text::UpsideDown;
-# ABSTRACT: Flip text upside-down using Unicode
-use charnames ':full';
+use charnames qw(:full);
 use Exporter qw(import);
-our @EXPORT  = ('upside_down');
+our @EXPORT = qw(upside_down);
+# ABSTRACT: Flip text upside-down using Unicode
+# VERSION
 
-# mapping taken from
-# http://www.fileformat.info/convert/text/upside-down-map.htm
+=head1 SYNOPSIS
+
+    use Text::UpsideDown;
+    print upside_down("marcel gruenauer");
+
+=head1 DESCRIPTION
+
+This module exports only one function: C<upside_down>.
+
+=cut
+
+# Mapping taken from:
+#  - http://www.fileformat.info/convert/text/upside-down-map.htm
+#  - http://en.wikipedia.org/wiki/Transformation_of_text
+#  - http://www.upsidedowntext.com/unicode
 #
 # Letters that don't change in upside-down view (like 'H', 'I') aren't given.
 our %upside_down_map = (
-    '!' => "\N{INVERTED EXCLAMATION MARK}",
-    '"' => "\N{DOUBLE LOW-9 QUOTATION MARK}",
-    '&' => "\N{TURNED AMPERSAND}",
-    "'" => ',',
-    '(' => "\N{RIGHT PARENTHESIS}",
-    '.' => "\N{DOT ABOVE}",
-    '/' => '\\',
-    '3' => "\N{LATIN CAPITAL LETTER OPEN E}",
-    '6' => '9',
-
-    # '7' => "\N{LATIN CAPITAL LETTER L WITH MIDDLE TILDE}",
-    ';'            => "\N{ARABIC SEMICOLON}",
-    '<'            => '>',
-    '?'            => "\N{INVERTED QUESTION MARK}",
     'A'            => "\N{FOR ALL}",
     'B'            => "\N{GREEK SMALL LETTER XI}",
     'C'            => "\N{ROMAN NUMERAL REVERSED ONE HUNDRED}",
@@ -37,7 +36,7 @@ our %upside_down_map = (
     'J'            => "\N{LATIN SMALL LETTER LONG S}",
     'K'            => "\N{RIGHT NORMAL FACTOR SEMIDIRECT PRODUCT}",
     'L'            => "\N{TURNED SANS-SERIF CAPITAL L}",
-    'M'            => 'W',
+    'M'            => "\N{LATIN EPIGRAPHIC LETTER INVERTED M}",
     'N'            => "\N{LATIN LETTER SMALL CAPITAL REVERSED N}",
     'P'            => "\N{CYRILLIC CAPITAL LETTER KOMI DE}",
     'Q'            => "\N{GREEK CAPITAL LETTER OMICRON WITH TONOS}",
@@ -46,8 +45,6 @@ our %upside_down_map = (
     'U'            => "\N{INTERSECTION}",
     'V'            => "\N{GREEK LETTER SMALL CAPITAL LAMDA}",
     'Y'            => "\N{TURNED SANS-SERIF CAPITAL Y}",
-    '['            => ']',
-    '_'            => "\N{OVERLINE}",
     'a'            => "\N{LATIN SMALL LETTER TURNED A}",
     'b'            => 'q',
     'c'            => "\N{LATIN SMALL LETTER OPEN O}",
@@ -67,33 +64,47 @@ our %upside_down_map = (
     'v'            => "\N{LATIN SMALL LETTER TURNED V}",
     'w'            => "\N{LATIN SMALL LETTER TURNED W}",
     'y'            => "\N{LATIN SMALL LETTER TURNED Y}",
-    '{'            => '}',
-    "\N{UNDERTIE}" => "\N{CHARACTER TIE}",
-    "\N{LEFT SQUARE BRACKET WITH QUILL}" =>
-      "\N{RIGHT SQUARE BRACKET WITH QUILL}",
+
+    '!' => "\N{INVERTED EXCLAMATION MARK}",
+    '"' => "\N{DOUBLE LOW-9 QUOTATION MARK}",
+    '&' => "\N{TURNED AMPERSAND}",
+    q{'} => ',',
+    '.' => "\N{DOT ABOVE}",
+    '/' => '\\',
+    '^' => "\N{LOGICAL OR}",
+    '*' => "\N{LOW ASTERISK}",
+    '1' => "\N{DOWNWARDS HARPOON WITH BARB RIGHTWARDS}",
+    '3' => "\N{LATIN CAPITAL LETTER OPEN E}",
+    '6' => '9',
+    '7' => "\N{BOPOMOFO LETTER ENG}",
+    ';' => "\N{ARABIC SEMICOLON}",
+    '?' => "\N{INVERTED QUESTION MARK}",
+    '(' => ')',
+    '[' => ']',
+    '{' => '}',
+    '<' => '>',
+    '_' => "\N{OVERLINE}",
+    "\N{UNDERTIE}"  => "\N{CHARACTER TIE}",
+    "\N{LEFT SQUARE BRACKET WITH QUILL}" => "\N{RIGHT SQUARE BRACKET WITH QUILL}",
     "\N{THEREFORE}" => "\N{BECAUSE}",
 );
 %upside_down_map = (%upside_down_map, reverse %upside_down_map);
+
+=head1 FUNCTIONS
+
+=head2 upside_down
+
+Takes a string and returns a string that looks like the characters have been
+turned upside-down and reversed so it could be read by turning your head
+upside-down. It uses special Unicode characters originally intended for
+mathematics and the like.
+
+=cut
 
 sub upside_down {
     my $text = shift;
     $text =~ s/(.)/ exists $upside_down_map{$1} ? $upside_down_map{$1} : $1/ge;
     join '' => reverse split '', $text;
 }
+
 1;
-
-=head1 SYNOPSIS
-
-    use Text::UpsideDown;
-    print upside_down("marcel gruenauer");
-
-=head1 DESCRIPTION
-
-This module exports only one function.
-
-=function upside_down
-
-Takes a string and returns a string that looks like the characters have been
-turned upside-down and reversed so it could be read by turning your head
-upside-down. It uses special Unicode characters originally intended for
-mathematics and the like.
