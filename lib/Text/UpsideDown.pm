78a179567ee9af02643668e78dd633204d18621c
Mike Doherty <doherty@cs.dal.ca>
2011-07-17 15:52:33 -0300

    Even faster - use tr///

--- lib/Text/UpsideDown.pm
+++ lib/Text/UpsideDown.pm
@@ -101,12 +101,13 @@ mathematics and the like.
 
 =cut
 
+my $tr = quotemeta join '', keys %upside_down_map;
+my $replace = quotemeta join '', values %upside_down_map;
 sub upside_down {
-    my $text = shift;
-    my $rx = sprintf '[%s]', quotemeta join '', keys %upside_down_map;
-    $rx = qr/($rx)/;
-    $text =~ s/$rx/$upside_down_map{$1}/g;
-    scalar reverse $text;
+    local $_ = shift;
+    eval "tr/$tr/$replace/"; ## no critic (BuiltinFunctions::ProhibitStringyEval)
+    die $@ if $@;
+    return scalar reverse;
 }
 
 1;
